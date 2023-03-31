a712ef86f293e351568a15fd2d110104195e2236
Eric <78090611+eric2702@users.noreply.github.com>
2023-03-31 09:49:11 +0700

    Update feed.js

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,19 +1,21 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
-    deferredPrompt.userChoice.then(function(choiceResult) {
+    deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === 'dismissed') {
-        console.log('User cancelled installation');
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
       } else {
-        console.log('User added to home screen');
+        console.log("User added to home screen");
       }
     });
 
@@ -22,9 +24,11 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+openCreatePostModal();
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
